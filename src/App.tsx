904d726247c104355c49ffa9fc07038ee64e1e32
BYAsurim <Pers0naL276276@gmail.com>
2023-08-04 09:10:22 +0300

    added useState and function deleteTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TasksType, Todolist} from "./Todolist";
 
 function App() {
 
-    const tasks1 = [
+    const [tasks,setTasks] = useState<Array<TasksType>>( [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "Redax", isDone: false },
+        { id: 5, title: "GraphQL", isDone: false }
+    ])
+
+    const deleteTask = (taskId:number) => {
+    setTasks(tasks.filter(t => t.id !== taskId))
+    }
+
 
     return (
         <div className="App">
-          <Todolist title={'What to learn???'} tasks={tasks1} />
-          <Todolist title={'Other things...'} tasks={tasks2}/>
+          <Todolist title={'What to learn???'}
+                    tasks={tasks}
+                    deleteTask = {deleteTask}
+          />
         </div>
     );
 }
