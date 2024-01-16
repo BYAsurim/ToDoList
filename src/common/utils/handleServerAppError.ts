32cd15493238dc08f9aba244511e0f10b735f8dd
BYAsurim <Pers0naL276276@gmail.com>
2024-01-16 12:57:10 +0300

    add utils functions

---
+++ src/common/utils/handleServerAppError.ts
@@ -0,0 +1,16 @@
+import {Dispatch} from "redux";
+import {setAppErrorAC} from "../../features/Application/appReducer";
+
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
+    if (data.messages.length) {
+        dispatch(setAppErrorAC(data.messages[0]));
+    } else {
+        dispatch(setAppErrorAC("Some error occurred"));
+    }
+};
+
+export type ResponseType<D = {}> = {
+    resultCode: number;
+    messages: Array<string>;
+    data: D;
+};
---
+++ src/common/utils/handleServerNetworkError.ts
@@ -0,0 +1,18 @@
+import {Dispatch} from "redux";
+import axios from "axios";
+import {setAppErrorAC} from "../../features/Application/appReducer";
+
+export const handleServerNetworkError = (err: unknown, dispatch: Dispatch): void => {
+    let errorMessage = "Some error occurred";
+
+    if (axios.isAxiosError(err)) {
+        errorMessage = err.response?.data?.message || err?.message || errorMessage;
+
+    } else if (err instanceof Error) {
+        errorMessage = `Native error: ${err.message}`;
+    } else {
+        errorMessage = JSON.stringify(err);
+    }
+    dispatch(setAppErrorAC(errorMessage))
+
+};
