7293fc92a5c11ebcc3f5a0fe2a83c47c5c55982a
BYAsurim <Pers0naL276276@gmail.com>
2025-01-20 16:39:10 +0300

    add token

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -5,5 +5,8 @@ export const instance = axios.create({
     withCredentials: true,
     headers: {
         'API-KEY': 'e7e3f008-e2dc-4435-835d-1184d4097cbd',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': '23b5595e-aea0-49dd-b0bd-985173c8779d'
     },
 })
