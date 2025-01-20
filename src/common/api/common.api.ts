e5a93d7252823f29a14820631848ba07c58808cd
BYAsurim <Pers0naL276276@gmail.com>
2025-01-20 17:56:35 +0300

    change home page

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -7,6 +7,5 @@ export const instance = axios.create({
         'API-KEY': 'e7e3f008-e2dc-4435-835d-1184d4097cbd',
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': '23b5595e-aea0-49dd-b0bd-985173c8779d'
     },
 })
